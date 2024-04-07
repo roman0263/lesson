f160cf5833080b1b610647b630a7b04f0e08992f
roman0263 <160611060+roman0263@users.noreply.github.com>
2024-04-07 19:22:45 +0300

    update dz

--- OB02/dz.py
+++ OB02/dz.py
@@ -1,10 +1,41 @@
 class User():
-    def __init__(self, id, name, access):
-        self.id = id
-        self.name = name
-        self.access = access
+    def __init__(self, user_id, name, level='user'):
+        self.__id = user_id
+        self.__name = name
+        self.__level = level
+
+    def get_id(self):
+        return self.__id
+
+    def get_name(self):
+        return self.__name
+
+    def get_access_level(self):
+        return self.__level
+
+    def set_name(self, name):
+        self.__name = name
+
 
 class Admin(User):
-    def __init__(self, id, name, access, admin):
-        super().__init__(id, name, access)
-        self.__private_admin = admin
+    def __init__(self, user_id, name):
+        super().__init__(user_id, name)
+        self.__level = "admin"
+
+    def add_user(self, user_list, user):
+        user_list.append(user)
+        print(f"пользователь добавлен")
+
+    def remove_user(self, user_list, user):
+        user_list.remove(user)
+        print(f"пользователь удален")
+
+user_list = []
+admin = Admin("99", "Roman")
+user1 = User("1", "Igor")
+user2 = User("2", "Oleg")
+
+print(user1.get_name())
+print(user2.get_name())
+admin.add_user(user_list, user1)
+admin.add_user(user_list, user2)
